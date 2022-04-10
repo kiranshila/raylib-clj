5124e1cb478593a105083a92b44de35ec67c7cd1
Kiran Shila <me@kiranshila.com>
2022-04-09 19:57:24 -0700

    Bigger benchmark, more volatile

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((clojure-mode . ((cider-clojure-cli-global-options . "-M:dev"))))
+((clojure-mode . ((cider-clojure-cli-global-options . "-A:dev"))))
--- examples/examples/textures/bunnymark.clj
+++ examples/examples/textures/bunnymark.clj
@@ -9,39 +9,34 @@
    [raylib.text.drawing :refer [draw-text! draw-fps!]]
    [raylib.core.drawing :as rcd]
    [raylib.shapes.basic :as rsb]
-   [raylib.colors :as colors]
-; TODO REMOVE
-   [taoensso.tufte :as tufte :refer (defnp p profiled profile)]))
+   [raylib.colors :as colors]))
 
-;; We'll request to send `profile` stats to `println`:
-(tufte/add-basic-println-handler! {})
+(def screen-width 1920)
+(def screen-height 1080)
 
-(def screen-width 800)
-(def screen-height 450)
-
-(def max-bunnies 50000)
+(def max-bunnies 500000)
 (def max-batch-elements 8192.0)
 
 (def bunny-texture (volatile! nil))
 (def bunnies (volatile! []))
 
-(defnp random-speed []
+(defn random-speed []
   {:x (/ (- (* (rand) 500) 250) 60)
    :y (/ (- (* (rand) 500) 250) 60)})
 
-(defnp random-color []
+(defn random-color []
   {:r (+ (rand-int 190) 50)
    :g (+ (rand-int 160) 80)
    :b (+ (rand-int 140) 100)
    :a 255})
 
-(defnp generate-bunny []
+(defn generate-bunny []
   (when (< (count @bunnies) max-bunnies)
     (vswap! bunnies conj {:position (rcm/get-mouse-position)
                           :speed (random-speed)
                           :color (random-color)})))
 
-(defnp update-bunny [bunny]
+(defn update-bunny [bunny]
   (let [screen-width (rcw/get-screen-width)
         screen-height (rcw/get-screen-height)
         {:keys [width height]} @bunny-texture
@@ -52,19 +47,16 @@
     (cond-> bunny
       true
       (update-in [:position :x] (partial + (get-in bunny [:speed :x])))
-
       true
       (update-in [:position :y] (partial + (get-in bunny [:speed :y])))
-
       (or (> x-off screen-width)
           (< x-off 0))
       (update-in [:speed :x] (partial * -1))
-
       (or (> y-off screen-height)
           (< y-off 0))
       (update-in [:speed :y] (partial * -1)))))
 
-(defnp draw-bunny [bunny]
+(defn draw-bunny [bunny]
   (rtd/draw-texture!
    @bunny-texture
    (get-in bunny [:position :x])
@@ -82,7 +74,7 @@
     (when (rcm/is-mouse-button-down? (mouse-button :left))
       (doseq [_ (range 100)]
         (generate-bunny)))
-    (vswap! bunnies (partial map update-bunny))
+    (vswap! bunnies (partial mapv update-bunny))
     (rcd/begin-drawing!)
     (rcd/clear-background! colors/raywhite)
     (doseq [bunny @bunnies]
